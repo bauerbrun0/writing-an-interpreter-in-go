4a3e53596b8b92a26d6f76e55cace4070bacdd30
bauerbrun0 <bauerbruno@protonmail.com>
2025-06-25 15:11:44 +0200

    feat(chapter-03): who's taking the trash out?

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/bauerbrun0/writing-an-interpreter-in-go
 
 go 1.23.5
-
-require github.com/goforj/godump v1.2.0 // indirect
--- main.go
+++ main.go
@@ -5,39 +5,14 @@ import (
 	"os"
 	"os/user"
 
-	"github.com/goforj/godump"
-
 	"github.com/bauerbrun0/writing-an-interpreter-in-go/repl"
 )
-
-type Profile struct {
-	Age   int
-	Email string
-}
-
-type User struct {
-	Name    string
-	Profile Profile
-	extra   string
-}
 
 func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-
-	user2 := User{
-		Name: "Alice",
-		Profile: Profile{
-			Age:   30,
-			Email: "alice@example.com",
-		},
-		extra: "asd",
-	}
-
-	// Pretty-print to stdout
-	godump.Dump(user2)
 
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")

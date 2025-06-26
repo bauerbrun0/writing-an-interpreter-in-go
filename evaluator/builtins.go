131f160092fa03502f5136231722bc55ed6e1524
bauerbrun0 <bauerbruno@protonmail.com>
2025-06-26 18:23:38 +0200

    feat(chapter-04): The Grand Finale

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/bauerbrun0/writing-an-interpreter-in-go/object"
+import (
+	"fmt"
+
+	"github.com/bauerbrun0/writing-an-interpreter-in-go/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -105,6 +109,14 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
